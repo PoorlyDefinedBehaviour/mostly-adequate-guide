66ffef7a724e8c07dd5f6e7cbba960147c763652
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-04-03 13:41:08 -0300

    feat: create curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c

---
+++ src/std.js
@@ -0,0 +1,8 @@
+export const curry = fn => {
+  const arity = fn.length
+
+  const internalCurry = (...args) =>
+    args.length < arity ? internalCurry.bind(null, ...args) : fn(...args)
+
+  return internalCurry
+}
